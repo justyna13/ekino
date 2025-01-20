df55eda303c7cfaf46b063c912e6a148bb773321
justynalodzinska <justyna.lodzinska@smartive.app>
2025-01-20 13:51:01 +0100

    prettier added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,20 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next';
+
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: "Ekino app",
-  description: "Ekino app to search and rate movies",
+	title: 'Ekino app',
+	description: 'Ekino app to search and rate movies',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body>{children}</body>
+		</html>
+	);
 }
